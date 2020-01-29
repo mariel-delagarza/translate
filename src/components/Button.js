5888aeb9e020d8f11d2fc56172a31fbab53b913a
mariel-delagarza <mariel.code@gmail.com>
2020-01-28 22:00:40 -0500

    Add contextType to hook up context object to class component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,8 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+  
   render() {
     return <button className="ui button primary">Submit</button>;
   }
