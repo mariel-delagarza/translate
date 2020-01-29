328b4bc13ecf1b1ce7a2a8259d482360ca7571d1
mariel-delagarza <mariel.code@gmail.com>
2020-01-28 21:45:03 -0500

    Add 2 flags

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        
+        <div>
+          Select a language:
+          <i className="flag us" />
+          <i className="flag nl" />
+        </div>
       </div>
     );
   }
