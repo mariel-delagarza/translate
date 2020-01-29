eca82ad97b8581bc3f642ac750a08939660cc276
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 15:36:40 -0500

    Remove contextType - not necessary when using consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
   
   render() {
     const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
