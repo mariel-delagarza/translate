97b1fd37634bbb84bf8bfa017c594670f49fbaec
mariel-delagarza <mariel.code@gmail.com>
2020-01-28 21:44:33 -0500

    Make div multi-line JSX

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
   state = { language: 'english' };
-  
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        
+      </div>
+    );
   }
 }
 
