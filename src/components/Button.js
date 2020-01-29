82ab344d39400f5643beb070ac1df1e0c925e5b2
mariel-delagarza <mariel.code@gmail.com>
2020-01-29 15:38:29 -0500

    Refactor to use consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,8 +4,13 @@ import LanguageContext from '../contexts/LanguageContext';
 class Button extends React.Component {
   
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
