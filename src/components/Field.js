acfce08cc7e00171dfd59117af39e227b1701114
mariel-delagarza <mariel.code@gmail.com>
2020-01-28 22:06:09 -0500

    wire up context

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+  
   render() {
     return (
       <div className="ui field">
